8f22085265f8e75ebc7c0dc3c72a9fc49fb8157a
Joshua Park <mp1781@scarletmail.rutgers.edu>
2025-01-07 02:39:18 -0500

    chore: work on promptpool

--- agentrec/datasets/promptpool.py
+++ agentrec/datasets/promptpool.py
@@ -1,3 +1,11 @@
 class PromptPool:
+    """
+    A class which generates and stores generated prompts from `Generator` in
+    order to allow proper cleaning and deduplication of data. This class is
+    serializable into a jsonl file which can be loaded at a later time.
+
+    `PromptPool` ensures that prompts are sorted by the specific agent to make
+    the manual cleaning of data more simple.
+    """
     def __init__(self):
         pass

4965868b11221459152359c120c5b840e36e09f3
Joshua Park <mp1781@scarletmail.rutgers.edu>
2025-01-08 19:01:19 -0500

    chore: add documentation for agent

--- agentrec/datasets/agent.py
+++ agentrec/datasets/agent.py
@@ -3,6 +3,9 @@ from typing import Optional
 
 @dataclass
 class Agent:
+    """
+    A dataclass which describes an AI agent.
+    """
     name: str
     description: Optional[str] = None
     examples: Optional[list[str] | list[dict]] = None

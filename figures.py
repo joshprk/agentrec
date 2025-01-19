e2afdce24f8de79ebb58d7cdaa076f2f5e1a7d40
Joshua Park <joshuprk@gmail.com>
2025-01-19 15:59:41 -0800

    chore: add scikit-learn

--- figures.py
+++ figures.py
@@ -1,5 +1,21 @@
+from dotenv import load_dotenv
+
+from agentrec.datasets import PromptPool
+from agentrec.models import SBERTAgentRec
+
+import math
+
 def main():
-    pass
+    pool = PromptPool()
+
+    pool.load(path="./data/prompts.jsonl",
+              agent_path="./data/agents.jsonl")
+
+    classifier = SBERTAgentRec("all-mpnet-base-v2")
+    classifier.fit(pool.pool)
+
+    embeddings = classifier.embeddings
 
 if __name__ == "__main__":
+    load_dotenv()
     main()

1f7640384a96b9d081c6dd3632f397e5b5505718
Joshua Park <joshuprk@gmail.com>
2025-01-19 16:16:52 -0800

    feat: create initial embeddings 3d pca

--- figures.py
+++ figures.py
@@ -1,4 +1,9 @@
 from dotenv import load_dotenv
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import pyplot as plt
+import numpy as np
 
 from agentrec.datasets import PromptPool
 from agentrec.models import SBERTAgentRec
@@ -15,6 +20,39 @@ def main():
     classifier.fit(pool.pool)
 
     embeddings = classifier.embeddings
+    visual_embeddings = {}
+
+    for agent in embeddings:
+        corpus = embeddings[agent]
+        scaler = StandardScaler()
+        pca    = PCA(n_components=3)
+
+        scaler.fit(corpus)
+        corpus = scaler.transform(corpus)
+
+        pca.fit(corpus)
+        visual_embeddings[agent] = pca.transform(corpus)
+
+    X = []
+    Y = []
+    label_map = []
+
+    for y in visual_embeddings:
+        if y not in label_map:
+            label_map.append(y)
+
+        for x in visual_embeddings[agent]:
+            X.append(x)
+            Y.append(label_map.index(y))
+
+    fig  = plt.figure()
+    axis = fig.add_subplot(111, projection="3d")
+
+    X = np.array(X)
+    Y = np.array(Y)
+
+    axis.scatter(X[:,0], X[:,1], X[:,2], c=Y, cmap="plasma")
+    fig.show()
 
 if __name__ == "__main__":
     load_dotenv()
